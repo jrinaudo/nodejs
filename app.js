a6f13447b121f706eda80b15b43e4ae95721d8e0
Juan Manuel Rinaudo <juan.rinaudo@jemstep.com>
2019-07-01 14:40:30 -0300

    Cookies

--- app.js
+++ app.js
@@ -15,26 +15,35 @@ const url = "mongodb://localhost:27018/conFusion";
 const connect = mongoose.connect(url);
 
 function auth(req, res, next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    next(err);
-    return;
-  }
-
-  var auth = new Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == "admin" && pass == "password") {
-    next(); // authorized
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+      return;
+    }
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == "admin" && pass == "password") {
+      res.cookie("user", "admin", { signed: true });
+      next(); // authorized
+    } else {
+      var err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+    }
   } else {
-    var err = new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    next(err);
+    if (req.signedCookies.user === "admin") {
+      next();
+    } else {
+      var err = new Error("You are not authenticated!");
+      err.status = 401;
+      next(err);
+    }
   }
 }
 
@@ -49,6 +58,7 @@ connect.then(
 
 var app = express();
 
+app.use(cookieParser("12345-67890-09876-54321"));
 app.use(auth);
 
 // view engine setup
